315175372ebd45e5664577c91e4ab0d8775a996d
CT <chenshifreedom@qq.com>
2024-04-09 13:38:59 +0800

    暂存

---
+++ CT.WPF.MagicEffects.Demo/ViewModels/TexturedColorReplace/TexturedColorReplaceEffectViewModel.cs
@@ -0,0 +1,11 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CT.WPF.MagicEffects.Demo.ViewModels.TexturedColorReplace {
+    partial class TexturedColorReplaceEffectViewModel : ObservableObject {
+    }
+}
